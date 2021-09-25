d97e11b038eec286e5ba75064c1acc8b6e794331
Alex Lecusay <90231709+AlexLecusay@users.noreply.github.com>
2021-09-25 13:56:33 -0400

    Commiting updates

--- src/App.js
+++ src/App.js
@@ -9,7 +9,16 @@ function App() {
        <p>Question 1</p>
        <div class="arrow arrow-right"></div>
      </header>
+     <div id="topName">
+       <p>Test</p>
+     </div>
      <QuestionBox />
+     <div id="bottomButtons">
+       <center>
+      <button id="Left">Quiz</button>
+      <button id="Right">Are you LatinX ?</button>
+      </center>
+     </div>
    </div>
   );
 }
--- src/QuestionBox.js
+++ src/QuestionBox.js
@@ -1,13 +1,13 @@
-import "./QuestionBox.css"
+import './QuestionBox.css';
 function QuestionBox(){
     return <div class="qb-surround">
         <section class="qb-section">
-            <h1 class="qb-question-title">What is React?</h1>
+            <h1 class="qb-question-title">What does LatinX stand for ?</h1>
             <section class="qb-question-choices">
-                <Answer text="A JS framework"/>
-                <Answer text="A Programming language"/>
-                <Answer text="A Keyboard"/>
-                <Answer text="A Human mechanism"/>
+                <Answer text="Mexican beer"/>
+                <Answer text="A branch language of Latin"/>
+                <Answer text="Latin American origin or descent"/>
+                <Answer text="X marks the spot !"/>
             </section>
         </section>
         <button class="qb-submit">Submit</button>
